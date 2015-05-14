1314573e56bbc04659e94febb63a6b521bbb4bda
Ery <erylei@gmail.com>
2015-05-14 18:47:52 +0800

    update Cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bottle'
+set :repo_url, 'git@github.com:ery/bottle.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
